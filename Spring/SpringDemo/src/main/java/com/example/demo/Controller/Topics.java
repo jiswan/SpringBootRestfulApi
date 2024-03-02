26c683b32696189341b9347039d620549c01c31c
jiswan <muhammedjiswan@gmail.com>
2024-03-02 12:32:47 -0500

    Spring boot 2ndUpdate

--- Spring/SpringDemo/src/main/java/com/example/demo/Controller/Topics.java
+++ Spring/SpringDemo/src/main/java/com/example/demo/Controller/Topics.java
@@ -1,15 +1,24 @@
 package com.example.demo.Controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.Model.Topic;
 
 @RestController
 public class Topics {
 	
 	@RequestMapping("/topics")
-	public String getAlltopics()
+	public List<Topic> getAlltopics()
 	{
-		return "All Topics";
+		return Arrays.asList(
+				new Topic("Spring", "SpringFramework", "SpringDescription"),
+				new Topic("Java", "CoreJava", "CoreJava  Description"),
+				new Topic("JavaScript", "JavaScript", "JavaScript Description")
+				);
 	}
 
 }
