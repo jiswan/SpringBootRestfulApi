094a0dbc3bc7c84b6410531e24d0fe26d7b398e0
jiswan <muhammedjiswan@gmail.com>
2024-03-13 22:17:06 -0400

    Register User

--- Spring/banknameweb/src/main/java/com/bankname/banknameweb/Controller/UserController.java
+++ Spring/banknameweb/src/main/java/com/bankname/banknameweb/Controller/UserController.java
@@ -2,6 +2,8 @@ package com.bankname.banknameweb.controller;
 
 import com.bankname.banknameweb.dto.UserResponse;
 import com.bankname.banknameweb.entity.User;
+import com.bankname.banknameweb.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,9 +12,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 
+    @Autowired
+    private UserService userService;
     @PostMapping("/register")
     public ResponseEntity<UserResponse> register(@RequestBody User user)
     {
+           User registeredUser = userService.registerUser(user);
+           UserResponse userResponse = new UserResponse();
+        userResponse.setName(registeredUser.getName());
+        userResponse.setEmail(registeredUser.getEmail());
+        userResponse.setAccountNumber(registeredUser.getAccount().getAccountNumber());
+        userResponse.setIFSC_code(registeredUser.getAccount().getIFSC_code());
+        userResponse.setBranch(registeredUser.getAccount().getBranch());
+        userResponse.setAccount_type(registeredUser.getAccount().getAccount_type());
 
+        return ResponseEntity.ok(userResponse);
     }
 }
--- Spring/banknameweb/src/main/java/com/bankname/banknameweb/Repository/AccountRepository.java
+++ Spring/banknameweb/src/main/java/com/bankname/banknameweb/Repository/AccountRepository.java
@@ -4,4 +4,5 @@ import com.bankname.banknameweb.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccountRepository extends JpaRepository<Account,Long> {
+    Account findByAccountNumber(String accountNumber);
 }
